914120766fa57f50135b35cb63b76e7c77cfd39f
Mustafa Cakir <mcakir@hurriyet.com.tr>
2018-12-10 14:49:40 +0300

    Shot stats groups are merged, custom sorting applied

--- src/components/eventdetails/Stats.js
+++ src/components/eventdetails/Stats.js
@@ -1,15 +1,37 @@
 import React, {Component} from 'react';
 
 class Stats extends Component {
+    constructor(props) {
+        super(props);
+        this.groups = [];
+    };
 
+    componentDidMount() {
+        const {eventData} = this.props;
+        const customSorting = ["Possession", "Shots", "Passes", "TVData", "Duels", "Defending"];
+        this.groups = eventData.statistics.periods[0].groups;
+        this.groups = this.groups.reduce((total, item) => {
+            if (item.groupName === "Shots extra") {
+                let shotIndex = total.findIndex(item => item.groupName === "Shots");
+                item.statisticsItems.forEach((item) => {
+                    total[shotIndex].statisticsItems.push(item);
+                });
+            } else {
+                item['sorting'] = customSorting.indexOf(item.groupName) !== -1 ? customSorting.indexOf(item.groupName) : 99;
+                total.push(item);
+            }
+            return total;
+        }, []);
+        this.groups.sort((a, b) => (a.sorting > b.sorting) ? 1 : ((b.sorting > a.sorting) ? -1 : 0));
+    };
 
     render() {
-        const {eventData} = this.props;
+
         return (
             <div>
                 <div className="stats container">
                     <div className="white-box mt-2 pb-2">
-                        {eventData.statistics.periods[0].groups.map((group, index) => {
+                        {this.groups.map((group, index) => {
                             if (group.groupName === "Possession") {
                                 return (
                                     <div key={index}>
@@ -56,7 +78,7 @@ class Stats extends Component {
                                 return (
                                     <div key={index}>
                                         <div className="group-title" key={index}>
-                                            {group.groupName}
+                                            {group.groupName === "TVData" ? "Discipline" : group.groupName}
                                         </div>
                                         {group.statisticsItems.map((item, index) => {
                                             let homeNum = parseInt(item.home, 10),
@@ -66,22 +88,30 @@ class Stats extends Component {
                                             return (
                                                 <div className="horizontal-bar style-2 mb-3" key={index}>
                                                     <div className="row labels mb-2">
-                                                        <div className="col text-left f-700"><span className={"num home-num " + (homeNum > awayNum ? "higher" : "")}>{item.home}</span></div>
+                                                        <div className="col text-left f-700"><span
+                                                            className={"num home-num " + (homeNum > awayNum ? "higher" : "")}>{item.home}</span>
+                                                        </div>
                                                         <div className="col f-300">{item.name}</div>
-                                                        <div className="col text-right f-700"><span className={"num away-num " + (awayNum > homeNum ? "higher" : "")}>{item.away}</span></div>
+                                                        <div className="col text-right f-700"><span
+                                                            className={"num away-num " + (awayNum > homeNum ? "higher" : "")}>{item.away}</span>
+                                                        </div>
                                                     </div>
                                                     <div className="row">
                                                         <div className="col">
                                                             <div className="row col-container">
-                                                                <div className="col"style={{maxWidth: 100 - homeWidth + "%"}}/>
-                                                                <div className="col home"  style={{maxWidth: homeWidth + "%"}}/>
+                                                                <div className="col"
+                                                                     style={{maxWidth: 100 - homeWidth + "%"}}/>
+                                                                <div className="col home"
+                                                                     style={{maxWidth: homeWidth + "%"}}/>
                                                             </div>
                                                         </div>
                                                         <div className="col col-space"/>
                                                         <div className="col">
                                                             <div className="row col-container">
-                                                                <div className="col away"  style={{maxWidth: awayWidth + "%"}}/>
-                                                                <div className="col" style={{maxWidth: 100 - awayWidth + "%"}}/>
+                                                                <div className="col away"
+                                                                     style={{maxWidth: awayWidth + "%"}}/>
+                                                                <div className="col"
+                                                                     style={{maxWidth: 100 - awayWidth + "%"}}/>
                                                             </div>
                                                         </div>
                                                     </div>
