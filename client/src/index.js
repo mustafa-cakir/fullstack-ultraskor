0878465f6416a4a5178347b479d332480929a999
Mustafa Cakir <mcakir@hurriyet.com.tr>
2019-01-14 10:11:30 +0300

    serviceWorker is unregistered

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import 'react-app-polyfill/jsdom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import './translations';
 
@@ -11,4 +11,7 @@ ReactDOM.render((
             <App />
         </BrowserRouter>
 ), document.getElementById('root'));
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
