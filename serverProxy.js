4378797f3ed6ca042ee88e3f814ff81ad09ff6cf
Mustafa Ckr <mc@flexiblewebdesign.com>
2019-01-28 23:06:55 +0300

    route fix

--- serverProxy.js
+++ serverProxy.js
@@ -5,9 +5,13 @@ const app = express();
 
 // define a simple route
 app.get('*', (req, res) => {
-    console.log(req);
-    const options = {
-        url: 'https://widgets.sir.sportradar.com/' + req.originalUrl,
+
+    let path = req.originalUrl;
+	path = path.substring(6, path.length);
+	console.log(path);
+
+	const options = {
+        url: 'https://widgets.sir.sportradar.com/' + path,
         headers: {
             'Referer': 'https://www.aspor.com.tr',
             'Origin': 'https://www.aspor.com.tr',
@@ -15,16 +19,16 @@ app.get('*', (req, res) => {
         }
     };
 
-    if (req.originalUrl.indexOf('common_widgets') > -1) {
+    if (path.indexOf('common_widgets') > -1) {
         options.url = 'https://www.ultraskor.com/static/live-match/common_widgets.js';
     }
     //console.log(options.url);
     request(options, function (error, response, body) {
         res.header('Access-Control-Allow-Origin', '*');
 
-        if (options.url.indexOf('translations') > -1) {
+        if (path.indexOf('translations') > -1) {
             res.header("Content-Type", "application/json; charset=utf-8");
-        } else if (options.url.indexOf('licensing') > -1) {
+        } else if (path.indexOf('licensing') > -1) {
             res.header("Content-Type", "text/plain; charset=utf-8");
         } else {
             res.header("Content-Type", "application/javascript; charset=utf-8");
