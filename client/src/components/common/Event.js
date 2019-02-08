095d8b131b357e95ad149e0d852a105161ad73c3
Mustafa Cakir <mcakir@hurriyet.com.tr>
2019-02-08 12:55:05 +0300

    SEO improvements on homepage

--- client/src/components/common/Event.js
+++ client/src/components/common/Event.js
@@ -6,121 +6,123 @@ import {Trans, withNamespaces} from "react-i18next";
 import {generateSlug} from "../../Helper";
 
 class Event extends Component {
-	isInProgress() {
-		let text;
-		let liveBlinkerCodes = [6, 7];
-		switch (this.props.event.status.type) {
-			case "inprogress":
-				text =
-					<div
-						className="red font-weight-bold">
-						<Trans>{this.props.event.statusDescription}</Trans>
-						{(this.props.event.status.code === 6) ? '' : ''}
-						{(liveBlinkerCodes.indexOf(this.props.event.status.code) > -1) ?
-							<span className="live-blinker">'</span> : ''}
-					</div>;
-				break;
-			case "notstarted":
-				text =
-					<div className="full-time font-weight-bold">
-						{moment(this.props.event.startTimestamp * 1000).isSame(moment(), 'day') ? "" : <div className="day">{moment(this.props.event.startTimestamp * 1000).format('D MMM')}</div>}
-						{moment(this.props.event.startTimestamp * 1000).format('HH:mm')}
-					</div>;
-				break;
-			case "canceled":
-				text =
-					<div className="red small-text line-clamp">
-						<Trans>Cancelled</Trans>
-					</div>;
-				break;
-			case "postponed":
-				text =
-					<div className="red small-text line-clamp">
-						<Trans>Postponed</Trans>
-					</div>;
-				break;
-			default:
-				text =
-					<div className="full-time font-weight-bold">
-						<Trans>FT</Trans>
-					</div>
-		}
-		return text;
-	}
+    isInProgress() {
+        let text;
+        let liveBlinkerCodes = [6, 7];
+        switch (this.props.event.status.type) {
+            case "inprogress":
+                text =
+                    <div
+                        className="red font-weight-bold">
+                        <Trans>{this.props.event.statusDescription}</Trans>
+                        {(this.props.event.status.code === 6) ? '' : ''}
+                        {(liveBlinkerCodes.indexOf(this.props.event.status.code) > -1) ?
+                            <span className="live-blinker">'</span> : ''}
+                    </div>;
+                break;
+            case "notstarted":
+                text =
+                    <div className="full-time font-weight-bold">
+                        {moment(this.props.event.startTimestamp * 1000).isSame(moment(), 'day') ? "" :
+                            <div className="day">{moment(this.props.event.startTimestamp * 1000).format('D MMM')}</div>}
+                        {moment(this.props.event.startTimestamp * 1000).format('HH:mm')}
+                    </div>;
+                break;
+            case "canceled":
+                text =
+                    <div className="red small-text line-clamp">
+                        <Trans>Cancelled</Trans>
+                    </div>;
+                break;
+            case "postponed":
+                text =
+                    <div className="red small-text line-clamp">
+                        <Trans>Postponed</Trans>
+                    </div>;
+                break;
+            default:
+                text =
+                    <div className="full-time font-weight-bold">
+                        <Trans>FT</Trans>
+                    </div>
+        }
+        return text;
+    }
 
-	favClickHandler() {
-		const event = this.props.event,
-			eventId = this.props.event.id;
+    favClickHandler() {
+        const event = this.props.event,
+            eventId = this.props.event.id;
 
-		let favEvents = this.props.favEvents || [],
-			favEventsList = this.props.favEventsList || [];
+        let favEvents = this.props.favEvents || [],
+            favEventsList = this.props.favEventsList || [];
 
-		if (favEvents.indexOf(eventId) < 0) {
-			favEvents.push(eventId);
-			favEventsList.push(event);
-		} else {
-			favEvents = favEvents.filter(item => item !== eventId);
-			favEventsList = favEventsList.filter(item => item !== event);
-		}
+        if (favEvents.indexOf(eventId) < 0) {
+            favEvents.push(eventId);
+            favEventsList.push(event);
+        } else {
+            favEvents = favEvents.filter(item => item !== eventId);
+            favEventsList = favEventsList.filter(item => item !== event);
+        }
 
-		localStorage.setItem('FavEvents', JSON.stringify(favEvents));
+        localStorage.setItem('FavEvents', JSON.stringify(favEvents));
 
-		if (this.props.updateParentState) {
-			this.props.updateParentState({
-				favEvents: favEvents,
-				favEventsList: favEventsList
-			})
-		}
-	}
+        if (this.props.updateParentState) {
+            this.props.updateParentState({
+                favEvents: favEvents,
+                favEventsList: favEventsList
+            })
+        }
+    }
 
-	render() {
-		const {event, t} = this.props;
-		let favEvents = this.props.favEvents || [];
-		const favActive = favEvents.indexOf(event.id) > -1;
-		return (
-			<div className={favActive ? "fav-active event-container " : "event-container"}>
-				<div className="container">
-					<div className="row">
-						<Link to={{
-							pathname: `/${t('match')}/${generateSlug(event.name)}-${t('live-score')}-${event.id}`,
-							state: {isPrev: true}
-						}} className="event-link col">
-							<div className="row">
-								<div className="col event-time pr-0 pl-2">
-									{this.isInProgress()}
-								</div>
-								<div className="col event-team home text-right pr-0 pl-2">
-									{event.homeRedCards ? <span className={"red-card"}>{event.homeRedCards}</span> : ""}
-									{event.homeTeam.name}
-								</div>
-								<div
-									className={"col event-score text-center font-weight-bold px-0" + (event.status.type === 'inprogress' ? ' live' : '')}>
-									{(typeof event.homeScore.current !== "undefined" || typeof event.awayScore.current !== "undefined") ?
-										(
-											<React.Fragment>
-												<span>{event.homeScore.current}</span><span className="score-separator">:</span><span>{event.awayScore.current}</span>
-											</React.Fragment>
-										)
-										: (" - ")
-									}
-								</div>
-								<div className="col event-team away text-left pl-0 pr-2">
-									{event.awayRedCards ? <span className={"red-card"}>{event.awayRedCards}</span> : ""}
-									{event.awayTeam.name}
-								</div>
-							</div>
-						</Link>
-						<div className="col event-fav pl-0 text-right pr-2" onClick={this.favClickHandler.bind(this)}>
-							{this.props.favContainer || favActive ? (
-								<Icon name="fas fa-star active"/>) : event.status.type !== "finished" ?
-								<Icon name="far fa-star"/> : ""}
-						</div>
-					</div>
+    render() {
+        const {event, t} = this.props;
+        let favEvents = this.props.favEvents || [];
+        const favActive = favEvents.indexOf(event.id) > -1;
+        return (
+            <div className={favActive ? "fav-active event-container " : "event-container"}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col event-time pr-0 pl-2">
+                            {this.isInProgress()}
+                        </div>
+                        <Link to={{
+                            pathname: `/${t('match')}/${generateSlug(event.name)}-${t('live-score')}-${event.id}`,
+                            state: {isPrev: true}
+                        }} className="event-link col p-0" title={`${event.homeTeam.name} - ${event.awayTeam.name}  ${t('click for live scores, lineup and stats')}`}>
+                            <div className="row m-0">
+                                <div className="col event-team home text-right pr-0 pl-2">
+                                    {event.homeRedCards ? <span className={"red-card"}>{event.homeRedCards}</span> : ""}
+                                    {event.homeTeam.name}
+                                </div>
+                                <div
+                                    className={"col event-score text-center font-weight-bold px-0" + (event.status.type === 'inprogress' ? ' live' : '')}>
+                                    {(typeof event.homeScore.current !== "undefined" || typeof event.awayScore.current !== "undefined") ?
+                                        (
+                                            <React.Fragment>
+                                                <span>{event.homeScore.current}</span><span
+                                                className="score-separator">:</span><span>{event.awayScore.current}</span>
+                                            </React.Fragment>
+                                        )
+                                        : (" - ")
+                                    }
+                                </div>
+                                <div className="col event-team away text-left pl-0 pr-2">
+                                    {event.awayRedCards ? <span className={"red-card"}>{event.awayRedCards}</span> : ""}
+                                    {event.awayTeam.name}
+                                </div>
+                            </div>
+                        </Link>
+                        <div className="col event-fav pl-0 text-right pr-2" onClick={this.favClickHandler.bind(this)}>
+                            {this.props.favContainer || favActive ? (
+                                <Icon name="fas fa-star active"/>) : event.status.type !== "finished" ?
+                                <Icon name="far fa-star"/> : ""}
+                        </div>
+                    </div>
 
-				</div>
-			</div>
-		)
-	}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default withNamespaces('translations')(Event)
--- client/src/components/common/Tournament.js
+++ client/src/components/common/Tournament.js
@@ -5,37 +5,39 @@ import {generateSlug, flagImg} from "../../Helper";
 import Link from "react-router-dom/es/Link";
 
 class Tournament extends Component {
-	render() {
-		const {t} = this.props;
-		return (
-			<React.Fragment>
-				{this.props.tournaments.map((tournament, i) => {
-					return (
-						<React.Fragment key={i}>
-							{/*<div className="tournament-container" data-key={i}>*/}
-								<div className="tournament-title league-link-container">
-									<Link to={{
-										pathname: `/${t('league')}/${generateSlug(t(tournament.category.name))}-${generateSlug(t(tournament.tournament.name))}${t('-standing-')}${tournament.tournament.uniqueId}${t('-season-')}${tournament.season ? tournament.season.id : "0"}`,
-										state: {isPrev: true}
-									}} className="row align-items-center league-link">
-										{flagImg(tournament)}
-										<div className="col tournament-name px-2">
-											<strong><Trans>{tournament.category.name}</Trans></strong> - {tournament.tournament.name}
-										</div>
-									</Link>
-								</div>
+    render() {
+        const {t} = this.props;
+        return (
+            <React.Fragment>
+                {this.props.tournaments.map((tournament, i) => {
+                    return (
+                        <React.Fragment key={i}>
+                            {/*<div className="tournament-container" data-key={i}>*/}
+                            <div className="tournament-title league-link-container">
+                                <div className="row align-items-center m-0 px-2">
+                                    {flagImg(tournament)}
+                                    <div className="col tournament-name px-2">
+                                        <Link to={{
+                                            pathname: `/${t('league')}/${generateSlug(t(tournament.category.name))}-${generateSlug(t(tournament.tournament.name))}${t('-standing-')}${tournament.tournament.uniqueId}${t('-season-')}${tournament.season ? tournament.season.id : "0"}`,
+                                            state: {isPrev: true}
+                                        }} className="league-link" title={`${tournament.category.name} - ${tournament.tournament.name}  ${t('click for standings, highlights and league fixtures')}`}>
+                                            <strong><Trans>{tournament.category.name}</Trans></strong> - {tournament.tournament.name}
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
 
-								{tournament.events.map((event, k) => {
-									return (<Event key={i+k} event={event}
-									               updateParentState={this.updateParentState} {...this.props}/>)
-								})}
-							{/*</div>*/}
-						</React.Fragment>
-					)
-				})}
-			</React.Fragment>
-		)
-	}
+                            {tournament.events.map((event, k) => {
+                                return (<Event key={i + k} event={event}
+                                               updateParentState={this.updateParentState} {...this.props}/>)
+                            })}
+                            {/*</div>*/}
+                        </React.Fragment>
+                    )
+                })}
+            </React.Fragment>
+        )
+    }
 }
 
 export default withNamespaces('translations')(Tournament)
