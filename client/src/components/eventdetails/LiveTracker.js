65fd1ed323733157dc702ba4f63e75b21c5e0f6f
Mustafa Cakir <mcakir@hurriyet.com.tr>
2019-03-25 15:51:02 +0300

    live tracking url problem fixed

--- client/src/components/eventdetails/LiveTracker.js
+++ client/src/components/eventdetails/LiveTracker.js
@@ -8,7 +8,7 @@ class LiveTracker extends Component {
 		if (!isTabLiveTracker) return false;
 		return (
 			<div className="live-match-iframe-wrapper">
-				<iframe title="Live Match Tracker" className="live-match-iframe" src={`/static/live-match/?matchid=${matchid}&lang=${language}`} width="100%" height="800" frameBorder="no" scrolling="no"/>
+				<iframe title="Live Match Tracker" className="live-match-iframe" src={`/static/live-match/index.html?matchid=${matchid}&lang=${language}`} width="100%" height="800" frameBorder="no" scrolling="no"/>
 			</div>
 		)
 	}
