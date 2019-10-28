3a2ba382372916a7dd7ddc3dedf6f4a4f1241281
Mustafa Ckr <mc@flexiblewebdesign.com>
2019-10-29 01:00:27 +0300

    fallback for legacy urls

--- server/fetch/eventdetails.js
+++ server/fetch/eventdetails.js
@@ -1,3 +1,4 @@
+const { convertToSofaScoreID } = require('../utils');
 const { fetchSofaScore } = require('./sofascore');
 const { fetchSportRadar } = require('./sportradar');
 const { fetchOleyWidget } = require('./oleyWidget');
@@ -7,29 +8,35 @@ const { cacheDuration } = require('../utils');
 
 const fetchEventDetails = (eventId, language) => {
     return getEventIds(eventId).then(ids => {
+        if (!ids.id_so) {
+            ids.id_so = eventId;
+        }
         const { min30, hour24 } = cacheDuration;
         return new Promise((resolve, reject) => {
-            const pAll = [
-                fetchSofaScore(`/event/${ids.id_so}/json`, min30).catch(err => console.log(err)),
-                fetchSportRadar(`/${language}/Europe:Istanbul/gismo/match_funfacts/${ids.id_sp}`, hour24).catch(() => null),
-                fetchOleyWidget(`teamstats/1/${ids.id_br}`, hour24).catch(() => null),
-                // fetchSofaScore(`/event/${ids.id_so}/lineups/json`, min30).catch(() => null),
-                fetchOleyWidget(`missings/1/${ids.id_br}`, hour24).catch(() => null),
-                // fetchSofaScore(`/event/${ids.id_so}/matches/json`, min30).catch(() => null)
-            ];
+            const pAll = [fetchSofaScore(`/event/${ids.id_so}/json`, min30).catch(err => console.log(err))];
 
+            if (ids.id_sp) {
+                pAll.push(
+                    fetchSportRadar(`/${language}/Europe:Istanbul/gismo/match_funfacts/${ids.id_sp}`, hour24).catch(
+                        () => null
+                    )
+                );
+            }
+            if (ids.id_br) {
+                pAll.push(fetchOleyWidget(`teamstats/1/${ids.id_br}`, hour24).catch(() => null));
+            }
+            if (ids.id_br) {
+                pAll.push(fetchOleyWidget(`missings/1/${ids.id_br}`, hour24).catch(() => null));
+            }
 
             Promise.all(pAll)
                 .then(responses => {
-                    const merged = mergeEventDetailsData(
-                        responses[0], // sofa
-                        responses[1], // radar
-                        responses[2], // oley
-                        responses[3], // sofaLineup
-                        // responses[4], // injuries
-                        // responses[5], // sofaMatches
-                        ids
-                    );
+                    const sofa = responses[0];
+                    const radar = ids.id_sp ? responses[1] : null;
+                    const oley = ids.id_br ? responses[2] : null;
+                    const injuries = ids.id_br ? responses[3] : null;
+
+                    const merged = mergeEventDetailsData(sofa, radar, oley, injuries, ids);
                     if (merged) {
                         resolve(merged);
                     } else {
