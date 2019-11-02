839920e41825c8c8ee6c78b9b950202c570eea2e
Mustafa Ckr <mc@flexiblewebdesign.com>
2019-11-03 00:19:12 +0300

    webpush related problem resolved
    webpush

--- server/services/webpush.service.js
+++ server/services/webpush.service.js
@@ -1,32 +1,32 @@
-const { firebase } = require('./firebase.service');
-const cacheService = require('./cache.service');
-const { isDev, t } = require('../utils');
+const { firebase } = require("./firebase.service");
+const cacheService = require("./cache.service");
+const { isDev, t } = require("../utils");
 
 exports.initWebPushByWebSocket = (oldEvent, newEvent) => {
     let type;
-    if (newEvent.status.code !== oldEvent.status.code) type = 'status_update';
-    if (newEvent.redCards.home > oldEvent.redCards.home) type = 'home_redcard';
-    if (newEvent.redCards.away > oldEvent.redCards.away) type = 'away_redcard';
-    if (newEvent.scores.home > oldEvent.scores.home) type = 'home_scored';
-    if (newEvent.scores.home < oldEvent.scores.home) type = 'home_scored_cancel';
-    if (newEvent.scores.away > oldEvent.scores.away) type = 'away_scored';
-    if (newEvent.scores.away < oldEvent.scores.away) type = 'away_scored_cancel';
+    if (newEvent.status.code !== oldEvent.status.code) type = "status_update";
+    if (newEvent.redCards.home > oldEvent.redCards.home) type = "home_redcard";
+    if (newEvent.redCards.away > oldEvent.redCards.away) type = "away_redcard";
+    if (newEvent.scores.home > oldEvent.scores.home) type = "home_scored";
+    if (newEvent.scores.home < oldEvent.scores.home) type = "home_scored_cancel";
+    if (newEvent.scores.away > oldEvent.scores.away) type = "away_scored";
+    if (newEvent.scores.away < oldEvent.scores.away) type = "away_scored_cancel";
     if (!type) return false;
 
     const { teams, id, scores, status, statusBoxContent } = newEvent;
     const message = {
         webpush: {
             headers: {
-                Urgency: 'high',
-                TTL: '900'
+                Urgency: "high",
+                TTL: "900"
             },
             notification: {
-                title: '',
-                body: '',
-                badge: 'https://www.ultraskor.com/favicon-32x32.png',
+                title: "",
+                body: "",
+                badge: "https://www.ultraskor.com/favicon-32x32.png",
                 vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40, 500],
-                color: '#0857bf',
-                icon: 'https://www.ultraskor.com/apple-touch-icon.png',
+                color: "#0857bf",
+                icon: "https://www.ultraskor.com/apple-touch-icon.png",
                 click_action: `https://www.ultraskor.com/eventdetails/${id}`
             },
             fcm_options: {
@@ -41,83 +41,83 @@ exports.initWebPushByWebSocket = (oldEvent, newEvent) => {
     const teamNames = `${homeTeamName} - ${awayTeamName}`;
     const teamNamesWithScore = `${homeTeamName} ${scores.home} - ${scores.away} ${awayTeamName}`;
 
-    if (type === 'status_update') {
+    if (type === "status_update") {
         if (status.code === 6) {
             // game started
             message.webpush.notification.title = `Maç Başladı`;
             message.webpush.notification.body = teamNames;
-            message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/start.mp3';
+            message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/start.mp3";
         } else if (status.code === 31) {
             // half time result
             message.webpush.notification.title = `İlk Yarı Sonucu`;
             message.webpush.notification.body = teamNamesWithScore;
-            message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/half-time.mp3';
+            message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/half-time.mp3";
         } else if (status.code === 7) {
             // 2nd half started
             message.webpush.notification.title = `İkinci Yarı Başladı`;
             message.webpush.notification.body = teamNamesWithScore;
-            message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/half-time.mp3';
+            message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/half-time.mp3";
         } else if (status.code === 100) {
             // full time result
             message.webpush.notification.title = `Maç Sonucu`;
             message.webpush.notification.body = teamNamesWithScore;
-            message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/finish.mp3';
+            message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/finish.mp3";
         } else if (status.code === 60) {
             // game postponed
             message.webpush.notification.title = `Maç Ertelendi`;
             message.webpush.notification.body = teamNames;
-            message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/cancel.mp3';
+            message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/cancel.mp3";
         } else if (status.code === 70) {
             // game cancelled
             message.webpush.notification.title = `Maç İptal Edildi`;
             message.webpush.notification.body = teamNames;
-            message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/cancel.mp3';
+            message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/cancel.mp3";
         }
-    } else if (type === 'away_redcard' || type === 'home_redcard') {
+    } else if (type === "away_redcard" || type === "home_redcard") {
         // red card
         message.webpush.notification.title = `KIRMIZI KART ${statusBoxContent}' ${
-            type === 'home_redcard' ? homeTeamName : awayTeamName
+            type === "home_redcard" ? homeTeamName : awayTeamName
         }`;
         message.webpush.notification.body = teamNamesWithScore;
         message.webpush.notification.icon = `https://www.ultraskor.com/images/team-logo/football_${
-            type === 'home_redcard' ? teams.home.id : teams.away.id
+            type === "home_redcard" ? teams.home.id : teams.away.id
         }.png`;
-        message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/red-card.mp3';
-    } else if (type === 'home_scored' || type === 'away_scored') {
+        message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/red-card.mp3";
+    } else if (type === "home_scored" || type === "away_scored") {
         //  goal scored
         message.webpush.notification.title = `GOL ${statusBoxContent}' ${
-            type === 'home_scored' ? homeTeamName : awayTeamName
+            type === "home_scored" ? homeTeamName : awayTeamName
         }`;
         message.webpush.notification.body = teamNamesWithScore;
         message.webpush.notification.icon = `https://www.ultraskor.com/images/team-logo/football_${
-            type === 'home_scored' ? teams.home.id : teams.away.id
+            type === "home_scored" ? teams.home.id : teams.away.id
         }.png`;
-        message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/goal.mp3';
-    } else if (type === 'home_scored_cancel' || type === 'away_scored_cancel') {
+        message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/goal.mp3";
+    } else if (type === "home_scored_cancel" || type === "away_scored_cancel") {
         // goal scored cancelled
-        message.webpush.notification.title = `GOL İPTAL ${type === 'home_scored_cancel' ? homeTeamName : awayTeamName}`;
+        message.webpush.notification.title = `GOL İPTAL ${type === "home_scored_cancel" ? homeTeamName : awayTeamName}`;
         message.webpush.notification.body = teamNamesWithScore;
         message.webpush.notification.icon = `https://www.ultraskor.com/images/team-logo/football_${
-            type === 'home_scored_cancel' ? teams.home.id : teams.away.id
+            type === "home_scored_cancel" ? teams.home.id : teams.away.id
         }.png`;
-        message.webpush.notification.sound = 'https://www.ultraskor.com/static/media/sound/cancel.mp3';
+        message.webpush.notification.sound = "https://www.ultraskor.com/static/media/sound/cancel.mp3";
     }
 
     if (message.webpush.notification.title) {
         const cacheKey = `/topics/match_${id}`;
         const cachedData = cacheService.instance().get(cacheKey);
-        if (typeof cachedData !== 'undefined') {
+        if (typeof cachedData !== "undefined") {
             // subscription found for this topic, send notification
-            console.log('subscription found, send the push for ', id);
+            console.log("subscription found, send the push for ", id);
             firebase
                 .messaging()
                 .send(message)
                 .then(response => {
                     // Response is a message ID string.
-                    console.log('Successfully sent message:', response);
+                    console.log("Successfully sent message. Type:", type, response);
                 })
                 .catch(error => {
-                    console.log('Error sending message:', error);
+                    console.log("Error sending message:", error);
                 });
         } else if (isDev) {
             console.log(
--- server/services/websocket.service.js
+++ server/services/websocket.service.js
@@ -1,20 +1,20 @@
-const WebSocket = require('ws');
-// const SocksProxyAgent = require('socks-proxy-agent');
-const { simplifyWebSocketData } = require('../utils');
-const { pushServiceChangesForWebPush } = require('./cronjob.service');
+const WebSocket = require("ws");
+const SocksProxyAgent = require("socks-proxy-agent");
+const { simplifyWebSocketData, isDev } = require("../utils");
+const { pushServiceChangesForWebPush } = require("./cronjob.service");
 
 let wsMaxRetry = 25;
 const init = io => {
     let swTimeout = null;
     const pushServiceUri = [
-        'wss://ws.sofascore.com:10017',
-        'wss://ws.sofascore.com:10011',
-        'wss://ws.sofascore.com:10012',
-        'wss://ws.sofascore.com:10014',
-        'wss://ws.sofascore.com:10013',
-        'wss://ws.sofascore.com:10016',
-        'wss://ws.sofascore.com:10015',
-        'wss://ws.sofascore.com:10010'
+        "wss://ws.sofascore.com:10017",
+        "wss://ws.sofascore.com:10011",
+        "wss://ws.sofascore.com:10012",
+        "wss://ws.sofascore.com:10014",
+        "wss://ws.sofascore.com:10013",
+        "wss://ws.sofascore.com:10016",
+        "wss://ws.sofascore.com:10015",
+        "wss://ws.sofascore.com:10010"
     ];
 
     const getPushServiceUri = pushServiceUri.sort(() => {
@@ -23,21 +23,21 @@ const init = io => {
 
     console.log(getPushServiceUri);
     const ws = new WebSocket(`${getPushServiceUri}/ServicePush`, {
-        origin: 'https://www.sofascore.com',
-        rejectUnauthorized: false
-        // ...{ agent: new SocksProxyAgent('socks://127.0.0.1:9050') }
+        origin: "https://www.sofascore.com",
+        rejectUnauthorized: false,
+        agent: new SocksProxyAgent("socks://127.0.0.1:9050")
     });
 
-    ws.on('error', err => {
-        console.log('errored', err);
+    ws.on("error", err => {
+        console.log("errored", err);
     });
 
-    ws.on('open', () => {
-        console.log('ws connected');
+    ws.on("open", () => {
+        console.log("ws connected");
         ws.send(
             JSON.stringify({
                 type: 0,
-                data: ['subscribe', { id: 'event', events: ['sport_football'] }]
+                data: ["subscribe", { id: "event", events: ["sport_football"] }]
             }),
             undefined,
             undefined
@@ -48,8 +48,8 @@ const init = io => {
         wsMaxRetry = 25;
     });
 
-    ws.on('close', err => {
-        console.log('ws disconnected. ', err);
+    ws.on("close", err => {
+        console.log("ws disconnected. ", err);
         if (wsMaxRetry > 0) init();
         clearInterval(swTimeout);
         wsMaxRetry -= 1;
@@ -59,15 +59,15 @@ const init = io => {
     //     // console.log('Ws pong ', data);
     // });
 
-    ws.on('message', res => {
-        if (res.substr(0, 15).match('pong')) {
+    ws.on("message", res => {
+        if (res.substr(0, 15).match("pong")) {
             // console.log('## pong recived', res);
         } else {
             if (!res) return false;
             // console.log(res);
             res = simplifyWebSocketData(res);
             pushServiceChangesForWebPush(res);
-            if (io) io.sockets.emit('push-service', res);
+            if (io) io.sockets.emit("push-service", res);
         }
         return false;
     });
